74298e5c7d82e8d530e7a694c4808e57cd758ca7
Joris Reynes <jo.r6349@gmail.com>
2025-01-30 22:24:01 +0100

    add healthcheck

--- product-trial-master-dotnet/Program.cs
+++ product-trial-master-dotnet/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using product_trial_master_dotnet.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +10,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddHealthChecks()
+    .AddCheck("database", () =>
+    {
+        using var connection = new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection"));
+        try
+        {
+            connection.Open();
+            return HealthCheckResult.Healthy("Connexion réussie à la base de données !");
+        }
+        catch (Exception ex)
+        {
+            return HealthCheckResult.Unhealthy($"Erreur de connexion : {ex.Message}");
+        }
+    });
+
 
 builder.Services.AddCors(options =>
 {
@@ -26,6 +45,20 @@ builder.Services.AddSwaggerGen();
 ;
 
 var app = builder.Build();
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = async (context, report) =>
+    {
+        context.Response.ContentType = "application/json";
+        var result = System.Text.Json.JsonSerializer.Serialize(new
+        {
+            status = report.Status.ToString(),
+            details = report.Entries.Select(e => new { key = e.Key, value = e.Value.Status.ToString() })
+        });
+        await context.Response.WriteAsync(result);
+    }
+});
 
 app.UseCors("AllowAngularApp");
 
